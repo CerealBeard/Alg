7c2296763100920717ebe8474139715af9dffc3e
CerealBeard <cereal.beard@yahoo.com>
2018-01-30 10:55:43 +0200

    Add files via upload

---
+++ Alg/src/nmmn.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class nmmn {
+
+    public static void main(String[] args) {
+	
+	int[] x = {5,5,8,2,1,4};
+	Arrays.sort(x);
+	
+	System.out.println(Arrays.toString(x));
+	
+	
+	
+    }
+}
